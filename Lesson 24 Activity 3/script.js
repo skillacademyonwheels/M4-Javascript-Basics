23ab5e32d65277a7292fc48a6dc9598d440bcba2
skillacademyonwheels <advelestica.design@gmail.com>
2025-08-16 09:17:12 +0530

    v1

---
+++ Lesson 24 Activity 3/script.js
@@ -0,0 +1,7 @@
+function myfunc() {
+    const myname = document.getElementById("name").value;
+    const myemail = document.getElementById("email").value;
+    const myage = document.getElementById("age").value;
+    const mymobile = document.getElementById("mobile").value;
+
+}
