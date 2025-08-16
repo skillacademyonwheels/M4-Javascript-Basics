18b3e68cb25bf3ed53c3b6a85b6f8add7bd352e8
skillacademyonwheels <advelestica.design@gmail.com>
2025-08-16 10:38:14 +0530

    v1

--- Lesson 24 Activity 3/script.js
+++ Lesson 24 Activity 3/script.js
@@ -3,5 +3,32 @@ function myfunc() {
     const myemail = document.getElementById("email").value;
     const myage = document.getElementById("age").value;
     const mymobile = document.getElementById("mobile").value;
+    const msgBox = document.getElementById("msgbox");
+
+    let message = '';
+
+    if (myemail == '') {
+        message = "Enter an Email";
+        msgBox.style.color = 'red';
+    }
+    else if (myname == '') {
+        message = "Enter your Name";
+        msgBox.style.color = 'red';
+    }
+    else if (myage === '' || myage == NaN || (myage < 0) || (myage > 120)) {
+        message = "Enter your Age Correctly";
+        msgBox.style.color = 'red';
+    }
+    else if (mymobile == '') {
+        message = "Enter Correct Mobile Number";
+        msgBox.style.color = 'red';
+    }
+    else {
+        message = "Login Successful";
+        msgBox.style.color = 'green';
+    }
+
+    msgBox.innerText = message;
+
 
 }
